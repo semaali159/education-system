4852041146ee75c921f2f5f66f2a62bb06433c3f
semaali159 <semaali1599@gmail.com>
2025-11-26 14:31:09 -0800

    docs add documentation for profiles

--- src/profiles/profile.controller.ts
+++ src/profiles/profile.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Post, Body, Param } from '@nestjs/common';
+import { Controller, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { ProfilesService } from './profile.service';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from 'src/common/enums/roles.enum';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+@ApiTags('Profiles')
+@ApiBearerAuth('access-token')
 @Controller('profiles')
 export class ProfilesController {
 constructor(private profilesService: ProfilesService) {}
 @Post('student/:userId')
+@UseGuards(AuthGuard('jwt'),RolesGuard)
+  @Roles(Role.STUDENT)
+    @ApiOperation({ summary: 'Create student profile' })
+  @ApiParam({ name: 'userId', type: Number, example: 12 })
+  @ApiBody({
+    schema: {
+      properties: {
+        gradeLevel: { type: 'string', example: 'Grade 10' },
+      },
+    },
+  })
+  @ApiResponse({ status: 201, description: 'Student profile created successfully' })
 createStudent(
 @Param('userId') userId: string,
 @Body() body: { gradeLevel?: string },
@@ -11,6 +30,18 @@ createStudent(
 return this.profilesService.createStudentProfile(+userId, body.gradeLevel);
 }
 @Post('teacher/:userId')
+@UseGuards(AuthGuard('jwt'),RolesGuard)
+@Roles(Role.TEACHER)
+  @ApiOperation({ summary: 'Create student profile' })
+  @ApiParam({ name: 'userId', type: Number, example: 12 })
+  @ApiBody({
+    schema: {
+      properties: {
+        specialty: { type: 'string', example: 'physics' },
+      },
+    },
+  })
+  @ApiResponse({ status: 201, description: 'Student profile created successfully' })
 createTeacher(
 @Param('userId') userId: string,
 @Body() body: { specialty?: string },
